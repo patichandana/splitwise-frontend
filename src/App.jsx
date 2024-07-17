7684e6930ef9cd4f5a5046a02111d7ec6ee030de
Pati Chandana <pati.c.chandana@oracle.com>
2024-07-17 07:40:44 +0530

    reverted unwanted changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,28 @@
 import "./App.css";
 import GroupsPage from "./pages/GroupsPage";
-import Group from "./pages/Group";
 import ExpensesPage from "./pages/ExpensesPage";
 import { useState } from "react";
 
 function App() {
-  // const [page, setPage] = useState('GroupsPage')
-  // const [params, setParams] = useState('');
-  // const props = {
-  //   page, setPage, params, setParams
-  // }
+  const [page, setPage] = useState('GroupsPage')
+  const [params, setParams] = useState('');
+  const props = {
+    page, setPage, params, setParams
+  }
 
-  // let loadComponent;
+  let loadComponent;
 
-  // if(page === 'GroupsPage') {
-  //   loadComponent = <GroupsPage {...props}/>
-  // } else if(page === 'ExpensesPage') {
-  //   loadComponent = <ExpensesPage {...props}/>
-  // }
+  if(page === 'GroupsPage') {
+    loadComponent = <GroupsPage {...props}/>
+  } else if(page === 'ExpensesPage') {
+    loadComponent = <ExpensesPage {...props}/>
+  }
 
-  // return (
-  //   <div className="App gradiant_background">
-  //     {loadComponent}
-  //   </div>
-  // );
-
-  const debts = [
-    {
-      "personName": "Riya",
-      "amount": "100",
-      'currency': "$"
-    },{
-      "personName": "Sena",
-      "amount": "100",
-      'currency': "$"
-    }
-  ];
-
-  const debts2 = [
-    {
-      "personName": "John",
-      "amount": "100",
-      'currency': "$"
-    },{
-      "personName": "Wade",
-      "amount": "100",
-      'currency': "$"
-    }
-  ]
-
-  return  <>
-  <Group debtsList={debts} totalBalance="200" currency="$"></Group>
-  <Group debtsList={debts2} totalBalance="0" currency="$"></Group>
-  <Group debtsList={debts2} totalBalance="-2008.9" currency="$"></Group>
-  </>;
+  return (
+    <div className="App">
+      {loadComponent}
+    </div>
+  );
 }
 
 export default App;
